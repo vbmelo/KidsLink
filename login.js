b9c57ec877cfdccc89ad034d09729939b7c83a17
Victor Barros Melo <33198775+vbmelo@users.noreply.github.com>
2021-01-15 20:56:29 +0000

    carrinho criado+ correcoes style login css

--- login.js
+++ login.js
@@ -3,134 +3,134 @@ function showLogin() {
 }
 
 
-        var emailArray=[];
-        var passwordArray=[];
+var emailArray=[];
+var passwordArray=[];
 
-        var loginBox = document.getElementById("login");
-        var regBox = document.getElementById("register");
-        var forgetBox = document.getElementById("forgot");
+var loginBox = document.getElementById("login");
+var regBox = document.getElementById("register");
+var forgetBox = document.getElementById("forgot");
 
-        var loginTab = document.getElementById("lt");
-        var regTab = document.getElementById("rt");
+var loginTab = document.getElementById("lt");
+var regTab = document.getElementById("rt");
 
-        function regTabFun(){
-            event.preventDefault();
+function regTabFun(){
+    event.preventDefault();
 
-            regBox.style.visibility="visible";
-            loginBox.style.visibility="hidden";
-            forgetBox.style.visibility="hidden";
+    regBox.style.visibility="visible";
+    loginBox.style.visibility="hidden";
+    forgetBox.style.visibility="hidden";
 
-            regTab.style.backgroundColor="rgb(12, 132, 189)";
-            loginTab.style.backgroundColor="rgba(11, 177, 224, 0.82)";
+    regTab.style.backgroundColor="rgb(12, 132, 189)";
+    loginTab.style.backgroundColor="rgba(11, 177, 224, 0.82)";
+}
+function loginTabFun(){
+    event.preventDefault();
+
+    regBox.style.visibility="hidden";
+    loginBox.style.visibility="visible";
+    forgetBox.style.visibility="hidden";
+
+    loginTab.style.backgroundColor="rgb(12, 132, 189)";
+    regTab.style.backgroundColor="rgba(11, 177, 224, 0.82)";
+}
+function forTabFun(){
+    event.preventDefault();
+
+    regBox.style.visibility="hidden";
+    loginBox.style.visibility="hidden";
+    forgetBox.style.visibility="visible";
+
+    regTab.style.backgroundColor="rgba(11, 177, 224, 0.82)";
+    loginTab.style.backgroundColor="rgba(11, 177, 224, 0.82)";
+
+}
+
+
+function register(){
+    event.preventDefault();
+
+    var email = document.getElementById("re").value;
+    var password = document.getElementById("rp").value;
+    var passwordRetype = document.getElementById("rrp").value;
+
+    if (email == ""){
+        alert("Email required.");
+        return ;
+    }
+    else if (password == ""){
+        alert("Password required.");
+        return ;
+    }
+    else if (passwordRetype == ""){
+        alert("Password required.");
+        return ;
+    }
+    else if ( password != passwordRetype ){
+        alert("Password don't match retype your Password.");
+        return;
+    }
+    else if(emailArray.indexOf(email) == -1){
+        emailArray.push(email);
+        passwordArray.push(password);
+
+        alert(email + "  Thanks for registration. \nTry to login Now");
+
+        document.getElementById("re").value ="";
+        document.getElementById("rp").value="";
+        document.getElementById("rrp").value="";
+    }
+    else{
+        alert(email + " is already register.");
+        return ;
+    }
+}
+function login(){
+    event.preventDefault();
+
+    var email = document.getElementById("se").value;
+    var password = document.getElementById("sp").value;
+
+    var i = emailArray.indexOf(email);
+
+    if(emailArray.indexOf(email) == -1){
+        if (email == ""){
+            alert("Email required.");
+            return ;
         }
-        function loginTabFun(){
-            event.preventDefault();
-
-            regBox.style.visibility="hidden";
-            loginBox.style.visibility="visible";
-            forgetBox.style.visibility="hidden";
-
-            loginTab.style.backgroundColor="rgb(12, 132, 189)";
-            regTab.style.backgroundColor="rgba(11, 177, 224, 0.82)";
+        alert("Email does not exist.");
+        return ;
+    }
+    else if(passwordArray[i] != password){
+        if (password == ""){
+            alert("Password required.");
+            return ;
         }
-        function forTabFun(){
-            event.preventDefault();
+        alert("Password does not match.");
+        return ;
+    }
+    else {
+        alert(email + " yor are login Now \n welcome to our website.");
 
-            regBox.style.visibility="hidden";
-            loginBox.style.visibility="hidden";
-            forgetBox.style.visibility="visible";
+        document.getElementById("se").value ="";
+        document.getElementById("sp").value="";
+        return ;
+    }
 
-            regTab.style.backgroundColor="rgba(11, 177, 224, 0.82)";
-            loginTab.style.backgroundColor="rgba(11, 177, 224, 0.82)";
+}
+function forgot(){
+    event.preventDefault();
 
+    var email = document.getElementById("fe").value;
+
+    if(emailArray.indexOf(email) == -1){
+        if (email == ""){
+            alert("Email required.");
+            return ;
         }
+        alert("Email does not exist.");
+        return ;
+    }
 
-
-        function register(){
-            event.preventDefault();
-
-            var email = document.getElementById("re").value;
-            var password = document.getElementById("rp").value;
-            var passwordRetype = document.getElementById("rrp").value;
-
-            if (email == ""){
-                alert("Email required.");
-                return ;
-            }
-            else if (password == ""){
-                alert("Password required.");
-                return ;
-            }
-            else if (passwordRetype == ""){
-                alert("Password required.");
-                return ;
-            }
-            else if ( password != passwordRetype ){
-                alert("Password don't match retype your Password.");
-                return;
-            }
-            else if(emailArray.indexOf(email) == -1){
-                emailArray.push(email);
-                passwordArray.push(password);
-
-                alert(email + "  Thanks for registration. \nTry to login Now");
-
-                document.getElementById("re").value ="";
-                document.getElementById("rp").value="";
-                document.getElementById("rrp").value="";
-            }
-            else{
-                alert(email + " is already register.");
-                return ;
-            }
-        }
-        function login(){
-            event.preventDefault();
-
-            var email = document.getElementById("se").value;
-            var password = document.getElementById("sp").value;
-
-            var i = emailArray.indexOf(email);
-
-            if(emailArray.indexOf(email) == -1){
-                if (email == ""){
-                    alert("Email required.");
-                    return ;
-                }
-                alert("Email does not exist.");
-                return ;
-            }
-            else if(passwordArray[i] != password){
-                if (password == ""){
-                    alert("Password required.");
-                    return ;
-                }
-                alert("Password does not match.");
-                return ;
-            }
-            else {
-                alert(email + " yor are login Now \n welcome to our website.");
-
-                document.getElementById("se").value ="";
-                document.getElementById("sp").value="";
-                return ;
-            }
-
-        }
-        function forgot(){
-            event.preventDefault();
-
-            var email = document.getElementById("fe").value;
-
-            if(emailArray.indexOf(email) == -1){
-                if (email == ""){
-                    alert("Email required.");
-                    return ;
-                }
-                alert("Email does not exist.");
-                return ;
-            }
-
-            alert("email is send to your email check it in 24hr. \n Thanks");
-            document.getElementById("fe").value ="";
-        }
+    alert("email is send to your email check it in 24hr. \n Thanks");
+    document.getElementById("fe").value ="";
+}
