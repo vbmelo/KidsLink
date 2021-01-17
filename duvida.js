aec1ca4a988e880332d7fbfe756787fe7b44329b
masquezorra <masquezorra@gmail.com>
2021-01-17 14:47:37 +0000

    duvidas a guardar info

--- duvida.js
+++ duvida.js
@@ -1,24 +1,44 @@
 if(localStorage.getItem("ArrayDuvidas")==null){
-    var ArrayDuvidas=[];
-    localStorage.setItem("ArrayDuvidas",ArrayDuvidas);
-    localStorage.setItem("index","0");
+    var ArrayDuvidas=["paulo@gmail.com","Paulo","Gostava que tivessem tema de naruto"];
+    localStorage.setItem("ArrayDuvidas",JSON.stringify(ArrayDuvidas));
+
 }
 
 
 
 function guardar(){
+
     var mail = document.getElementById("mail").value;
     var nome = document.getElementById("nome").value;
     var texto = document.getElementById("texto").value;
-    console.log(mail);
-    console.log(nome);
-    console.log(texto);
-    var NovaDuvida= [mail,nome,texto];
-    console.log(NovaDuvida);
-    ArrayDuvidas.push(NovaDuvida);
-    localStorage.settItem("ArrayDuvidas");
-    var i=localStorage.getItem(Index);
-    i++;
-    localStorage.setItem("index",i);
-    window.location = "index.html"; // Redireciona 
+    var ArrayDuvidas =JSON.parse(localStorage.getItem("ArrayDuvidas"));//ler os valores
+    ArrayDuvidas.push(mail);
+    ArrayDuvidas.push(nome);
+    ArrayDuvidas.push(texto);
+
+   
+    
+    localStorage.setItem("ArrayDuvidas",JSON.stringify(ArrayDuvidas));
+
+    var ArrayDuvidas =JSON.parse(localStorage.getItem("ArrayDuvidas"));//refresca var para fazer o for
+
+    console.log(ArrayDuvidas.length);
+    
+    for (var i = 0; i < ArrayDuvidas.length; i=i+3) {
+        
+        console.log("mail");
+        console.log(ArrayDuvidas[i]);
+
+        console.log("nome");
+        console.log(ArrayDuvidas[i+1]);
+
+        console.log("texto");
+        console.log(ArrayDuvidas[i+2]);
+        console.log(i);
+      } 
+
+
+
+    window.location = "duvidas.html"; // Redireciona 
+  
 }
